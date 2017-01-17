53393ef6b993faf10cd4391476884ee338dc597e
Alexbijmolt <alex.bijmolt@student.stenden.com>
2017-01-17 15:50:42 +0100

    Enkele dingetjes

--- Bootstrap/home.php
+++ Bootstrap/home.php
@@ -18,7 +18,6 @@
   <?php
   	include ('session.php');
 	/* include login sessions for the use of user information */
-        echo $_SESSION['portfolio'];
   ?>
   <body>
 
@@ -35,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php"> Stenden Portfolio </a>
+          <a class="navbar-brand" href="index.php"><img src="img/stendenlogo.png" alt="logo" width="8%"> Stenden Portfolio </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -121,7 +120,7 @@
                     }else {
                         echo '<li><a href="login.php"><i class="fa fa-power-off"></i> Log In</a></li>';
                     }
-                    ?> 
+                    ?>
             </li>
           </ul>
         </div><!-- /.navbar-collapse -->
@@ -130,46 +129,10 @@
       <div id="page-wrapper">
 
         <div class="row">
-          <div class="col-lg-12">
-            <h1>Frits Huig <small>Portfolio</small></h1>
-            <ol class="breadcrumb">
-              <li><a href="index.php"><i class="icon-dashboard"></i> Dashboard</a></li>
-              <li class="active"><i class="icon-file-alt"></i> Blank Page</li>
-            </ol>
-          </div>
             <div id="content">
-                <div class="info">
-					<div class="pasfoto">
-						<img src="img/Pasfoto.jpg" alt="pasfoto">
-					</div>
-                    <div class="gegevens">
-                        <h2>Personelijke gegevens</h2>
-                        <p>Adres: Ovaalstraat 33</p>
-                        <p>Woonplaats: ovaalland</p>
-                        <p>telefoon: 0591-543344</p>
-                        <p>E-mail: Frits.Huig@huigelaartje.com </p>
-                        <p>Leeftijd: 22 jaar</p>
-                        <p>Auto?: Ja</p>
-                    </div>
+                <div class="welkom">
+                    <h1>Welkom op het Portfolio Systeem van Stenden Hogeschool</h1>
                 </div>
-                <div class="overjouzelf">
-                    <h1>Welkom op mijn portfolio</h1>
-                    <p>Mijn naam is Frits Huigen. Ik ben afgestuddeerd op het potocollege ik hoop dat u door mijn portfolio meer over mij te weten komt.</p>
-                </div>
-				<div id="eigenfotos">
-					<div class="fototrots">
-						<img src="img/test1.jpg" alt="test">
-					</div>
-					<div class="fototrots">
-						<img src="img/test2.png" alt="test">
-					</div>
-					<div class="fototrots">
-						<img src="img/test3.png" alt="test">
-					</div>
-					<div class="fototrots">
-						<img src="img/test4.jpg" alt="test">
-					</div>
-				</div>
             </div>
         </div><!-- /.row -->
 
