ae115d8b66c985b0727565c8b15d8b0df72d4cbd
99ro <robindeboer1999@gmail.com>
2017-01-23 12:10:01 +0100

    Add files via upload

--- New folder/stendenportfolio-master/Bootstrap/content/fotogalerij.php
+++ New folder/stendenportfolio-master/Bootstrap/content/fotogalerij.php
@@ -6,28 +6,30 @@
             <ol class="breadcrumb">
               <li><a href="index.html"><i class="icon-dashboard"></i> Dashboard</a></li>
               <li class="active"><i class="icon-file-alt"></i> Blank Page</li>
-			  <li><a href="projectupload.php"><input type="submit" name="uploadproject" value="Upload a new project"></a></li>
+			  <li><a href="content/projectupload.php"><input type="submit" name="uploadproject" value="Upload a new project"></a></li>
             </ol>
           </div>
         </div><!-- /.row -->
 		<?php
+		if(isset($_SESSION['portfolio'])){
 		
-		$dir = 'projects/'.$_SESSION['login_user'].'/projectpicture/';
-		if(file_exists($dir) == FALSE){
-			echo 'Er is nog geen project geupload.';
-		}
-		else{
-			$files = scandir($dir);
-			
-			rsort($files);
-			foreach ($files as $file) {
-				if ($file != '.' && $file != '..') {
-					echo $file;
-					echo '<a href="projectbescrijving.php?Project=projects/'.$_SESSION['login_user'].'/'.$file.'"> >"<img src="' . $dir . $file . '"/></a>';
+		
+			$dir = 'projects/'.$row2['User_Email'].'/projectpicture/';
+			if(file_exists($dir) == FALSE){
+				echo 'Er is nog geen project geupload.';
+			}
+			else{
+				$files = scandir($dir);
+				
+				rsort($files);
+				foreach ($files as $file) {
+					if ($file != '.' && $file != '..') {
+						echo $file;
+						echo '<a href="projectbescrijving.php?Project=projects/'.$row2['User_Email'].'/'.$file.'"> >"<img src="' . $dir . $file . '"/></a>';
+					}
 				}
 			}
 		}
-		
 		
 		
 		
--- New folder/stendenportfolio-master/Bootstrap/content/projectupload.php
+++ New folder/stendenportfolio-master/Bootstrap/content/projectupload.php
@@ -1,115 +1,189 @@
+<?php
+session_start();
+?>
+
+
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
         <?php
-		if(!file_exists("projects/".$_SESSION['login_user'])){
-				
-			mkdir("projects/".$_SESSION['login_user']);
-		}
-		if(!file_exists("projects/".$_SESSION['login_user']."/projectpicture")){
-			mkdir("projects/".$_SESSION['login_user']."/projectpicture");
-		}
-		if(isset($_POST["submit1"])){
-			if($_SESSION['login_user'] == NULL){
-				echo "you must be logged in to upload a project";
+		
+			if(!file_exists("projects/".$_SESSION['login_user'])){
+				mkdir("projects/".$_SESSION['login_user']);
 			}
-		else{
-			if(empty($_POST['projectName'])){
-                echo "vul alles in";
-            }
-			else{
-				$projectName = $_POST['projectName'];
-				if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/png"))){
-				
-					if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "text/css") || ($_FILES["file"]["type"] == "text/html") || ($_FILES["file"]["type"] == "audio/mpeg3")
-					|| ($_FILES["file"]["type"] == "application/pdf")  || ($_FILES["file"]["type"] == "application/mspowerpoint")  || ($_FILES["file"]["type"] == "application/vnd.ms-powerpoint")  || ($_FILES["file"]["type"] == "text/plain")  || ($_FILES["file"]["type"] == "application/excel")
-					|| ($_FILES["file"]["type"] == "application/zip")  || ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")  || ($_FILES["file"]["type"] == "text/x-php") || ($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "text/x-php")  )){
-						
-						if ($_FILES["file"]["error"] > 0)
-						{
-							echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-						} else{
-							//echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-							//echo '<br>'. $_FILES["file"]["type"] . "<br />";
-							$array = explode("/", $_FILES["file"]["type"]);
-							$extension = $array[1];
-							//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-							//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-							
-							
-							move_uploaded_file($_FILES["file"]["tmp_name"], "projects/".$_SESSION['login_user']."/projectpicture/".$projectName.".".$extension);
-							//echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-							if(!file_exists("projects/".$_SESSION['login_user']."/".$projectName)){
-							mkdir("projects/".$_SESSION['login_user']."/".$projectName);
-							
-
-						
-						$total = count($_FILES['upload']['name']);
-
-						// Loop through each file
-						for($i=0; $i<$total; $i++) {
-						  //Get the temp file path
-						  $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
-
-						  //Make sure we have a filepath
-						  if ($tmpFilePath != ""){
-							//Setup our new file path
-							$newFilePath = "projects/".$_SESSION['login_user']."/".$projectName."/". $_FILES['upload']['name'][$i];
-
-							//Upload the file into the temp dir
-							if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-
-                                $DBConnect = mysqli_connect("localhost", "root", "");
-                                if ($DBConnect === FALSE){
-                                    echo "<p>Unable to connect to the database server.</p>" . "<p>Error code " . mysqli_error() . ": " . mysqli_error() . "</p>";
-                                } else{
-                                    $DBName = "project_portfolio";
-									if (!mysqli_select_db($DBConnect, $DBName)){
-                                        echo "<p>error</p>";
-                                    }else{
-                                        $TableName = "projects";
-                                        $SQLstring = "INSERT INTO project (User_ID, Project_Name, SLB_Opdracht) VALUES('".$_SESSION['login_user']."', '".$projectName."', '".$_POST['SLB']."')";
-                                        mysqli_query($DBConnect, $SQLstring);
-
-
-                                    }
-                                    mysqli_close($DBConnect);
-                                }                                                            
-                                                            
-                                                            
-                                                            
-                                                            
-                                                            
-                                                            
-                                                            
-							}
-							else{die;}
-						  }
-						  else{die;}
-						}
-							}
-							else{}
-						
-
-					
-						}
-						
-						echo "uw project is geupload";
+			if(!file_exists("projects/".$_SESSION['login_user']."/projectpicture")){
+				mkdir("projects/".$_SESSION['login_user']."/projectpicture");
+			}
+			if(!file_exists("projects/".$_SESSION['login_user']."/SLB")){
+				mkdir("projects/".$_SESSION['login_user']."/SLB");
+			}
+			if(!file_exists("projects/".$_SESSION['login_user']."/SLB/projectpicture")){
+				mkdir("projects/".$_SESSION['login_user']."/SLB/projectpicture");
+			}
+			
+			
+			
+			if(isset($_POST["submit1"])){
+				if($_SESSION['login_user'] == NULL){
+					echo "you must be logged in to upload a project";
+				}
+				else{
+					if(empty($_POST['projectName'])){
+						echo "vul alles in";
 					}
 					else{
-						echo "Uw bestand type bestand mag niet geupload worden.";
-					}
+						$projectName = $_POST['projectName'];
+						if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/png"))){
+						
+							if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "text/css") || ($_FILES["file"]["type"] == "text/html") || ($_FILES["file"]["type"] == "audio/mpeg3")
+							|| ($_FILES["file"]["type"] == "application/pdf")  || ($_FILES["file"]["type"] == "application/mspowerpoint")  || ($_FILES["file"]["type"] == "application/vnd.ms-powerpoint")  || ($_FILES["file"]["type"] == "text/plain")  || ($_FILES["file"]["type"] == "application/excel")
+							|| ($_FILES["file"]["type"] == "application/zip")  || ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")  || ($_FILES["file"]["type"] == "text/x-php") || ($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "text/x-php")  )){
+								
+								if($_POST['SLB'] == '1'){
+									if ($_FILES["file"]["error"] > 0)
+									{
+										echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+									} else{
+										//echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+										//echo '<br>'. $_FILES["file"]["type"] . "<br />";
+										$array = explode("/", $_FILES["file"]["type"]);
+										$extension = $array[1];
+										//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+										//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+										
+										
+										move_uploaded_file($_FILES["file"]["tmp_name"], "projects/".$_SESSION['login_user']."/SLB/projectpicture/".$projectName.".".$extension);
+										//echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+										if(!file_exists("projects/".$_SESSION['login_user']."/SLB/".$projectName)){
+										mkdir("projects/".$_SESSION['login_user']."/SLB/".$projectName);
+									
 
+								
+										$total = count($_FILES['upload']['name']);
+
+										// Loop through each file
+										for($i=0; $i<$total; $i++) {
+										  //Get the temp file path
+										  $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+										  //Make sure we have a filepath
+											if ($tmpFilePath != ""){
+												//Setup our new file path
+												$newFilePath = "projects/".$_SESSION['login_user']."/SLB/".$projectName."/". $_FILES['upload']['name'][$i];
+
+												//Upload the file into the temp dir
+												if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+
+													$DBConnect = mysqli_connect("localhost", "root", "");
+													if ($DBConnect === FALSE){
+														echo "<p>Unable to connect to the database server.</p>" . "<p>Error code " . mysqli_error() . ": " . mysqli_error() . "</p>";
+													} else{
+														$DBName = "project_portfolio";
+														if (!mysqli_select_db($DBConnect, $DBName)){
+															echo "<p>error</p>";
+														}else{
+															$TableName = "projects";
+															$SQLstring = "INSERT INTO projects (User_Email, Project_Name, SLB_Opdracht) VALUES('".$_SESSION['login_user']."', '".$projectName."', '1')";
+															mysqli_query($DBConnect, $SQLstring);
+
+
+														}
+														mysqli_close($DBConnect);
+													}                                                            
+												}									
+											}
+										}
+									}
+									}
+								}
+								else{
+							
+							
+									if ($_FILES["file"]["error"] > 0)
+									{
+										echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+									} else{
+										//echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+										//echo '<br>'. $_FILES["file"]["type"] . "<br />";
+										$array = explode("/", $_FILES["file"]["type"]);
+										$extension = $array[1];
+										//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+										//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+										
+										
+										move_uploaded_file($_FILES["file"]["tmp_name"], "projects/".$_SESSION['login_user']."/projectpicture/".$projectName.".".$extension);
+										//echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+										if(!file_exists("projects/".$_SESSION['login_user']."/".$projectName)){
+										mkdir("projects/".$_SESSION['login_user']."/".$projectName);
+									
+
+								
+								$total = count($_FILES['upload']['name']);
+
+								// Loop through each file
+								for($i=0; $i<$total; $i++) {
+								  //Get the temp file path
+								  $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+								  //Make sure we have a filepath
+								  if ($tmpFilePath != ""){
+									//Setup our new file path
+									$newFilePath = "projects/".$_SESSION['login_user']."/".$projectName."/". $_FILES['upload']['name'][$i];
+
+									//Upload the file into the temp dir
+									if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+
+										$DBConnect = mysqli_connect("localhost", "root", "");
+										if ($DBConnect === FALSE){
+											echo "<p>Unable to connect to the database server.</p>" . "<p>Error code " . mysqli_error() . ": " . mysqli_error() . "</p>";
+										} else{
+											$DBName = "project_portfolio";
+											if (!mysqli_select_db($DBConnect, $DBName)){
+												echo "<p>error</p>";
+											}else{
+												$TableName = "projects";
+												$SQLstring = "INSERT INTO project (User_ID, Project_Name, SLB_Opdracht) VALUES('".$_SESSION['login_user']."', '".$projectName."', '0')";
+												mysqli_query($DBConnect, $SQLstring);
+
+
+											}
+											mysqli_close($DBConnect);
+										}                                                            
+									}									
+								  								
+																
+																
+																
+																
+																
+								}
+								
+							 }
+						  
+							}
+								}
 				
+							
+
+						
+							}
+							
+							echo "uw project is geupload";
+						}
+						else{
+							echo "Uw bestand type bestand mag niet geupload worden.";
+						}
+
+					
+					}	
+					else{
+						echo "Alleen afbeeldingen kunnen geupload worden.";
+					}
+					
+					}
 				}	
-				else{
-					echo "Alleen afbeeldingen kunnen geupload worden.";
-				}
-				
-				
-		}}	
-		}
-		//}
+			}
+		
         ?>
 		<form action="#" method="POST" enctype="multipart/form-data">
 			<p>Project name :</p>
